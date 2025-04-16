cee2e9bc48f1dd32cb666bf75a2b6643653f1792
KevinCz <lenguyenhoangthanh123@gmail.com>
2025-04-16 11:08:15 +0700

    Fix missing permission check for commands

--- src/main/java/net/pixeldreamstudios/kevslibrary/util/RPGUtil.java
+++ src/main/java/net/pixeldreamstudios/kevslibrary/util/RPGUtil.java
@@ -20,7 +20,7 @@ public class RPGUtil
     {
         dispatcher.register(
                 CommandManager.literal("rpgutility")
-
+                        .requires(source -> source.hasPermissionLevel(2))
                         // ✅ /rpgutility after
                         .then(
                                 CommandManager.literal("after")
